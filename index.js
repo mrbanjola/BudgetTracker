006614b618967e789524cddfb80f6ef0b83aca5d
Karl Sjöholm <421414-KalleSjoholm1@users.noreply.replit.com>
2024-11-13 00:30:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,105 @@
+var socketio = require('socket.io');
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const fileUpload = require('express-fileupload');
+const request = require('request');
+const Database = require("@replit/database");
+const db = new Database();
+require('./utils.js');
+
+const salaryPeriod = GetSalaryPeriod();
+
+
+
+const app = express();
+
+function sendWebRequestToAPI(url, callback) {
+	console.log("sending request")
+    request(url, (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+			console.log("we don't have an error");
+			callback(JSON.parse(body).data);
+
+        } else {
+			console.log("We have problem!!")
+            callback(error || new Error('Request to API failed'), null);
+        }
+    });
+}
+const url = process.env['API_URL']
+sendWebRequestToAPI(url,(data) => {
+	db.set("expenses",data);
+} )
+
+
+
+var server = http.createServer(app);
+var io = socketio(server);
+
+const publicDirectoryPath = path.join(__dirname, './public');
+
+app.use(express.static(publicDirectoryPath));
+
+
+
+
+
+io.on('connection', socket => {
+	console.log('User connected');
+
+		GetExpensesForCurrentPeriod((expenses) => {
+		socket.emit("expenses",expenses.filter(expense => expense.salaryPeriod == salaryPeriod.salaryPeriod));
+	});
+
+	socket.emit("salaryPeriod",salaryPeriod);
+
+
+});
+
+server.listen(port, '0.0.0.0');
+
+
+async function GetExpensesForCurrentPeriod(callback) {
+	db.get("expenses").then((expenses) => {
+		callback(JSON.parse(expenses.value));
+	})
+};
+
+function GetSalaryPeriod() {
+	let payDayThisMonth = new Date();
+	payDayThisMonth.setDate(25);
+
+	// Om söndag eller lördag
+	var badDays = [0, 6]
+	if (badDays.includes(payDayThisMonth.getDay())) {
+	  if (payDayThisMonth.getDay() == 0) {
+		payDayThisMonth = payDayThisMonth.addDays(-2);
+	  } else {
+		payDayThisMonth = payDayThisMonth.addDays(-1);
+	  }
+	}
+
+	if (payDayThisMonth <= new Date()) {
+	  return {
+	    salaryPeriod: payDayThisMonth.getMonth(),
+	    salaryDate: payDayThisMonth
+	  };
+	} else {
+	  let payDayLastMonth = new Date(new Date().addMonths(-1));
+	  payDayLastMonth.setDate(25);
+
+	  if (badDays.includes(payDayLastMonth.getDay())) {
+		if (payDayLastMonth.getDay() == 0) {
+		  payDayLastMonth = payDayLastMonth.addDays(-2);
+		} else {
+		  payDayLastMonth = payDayLastMonth.addDays(-1);
+		}
+	  };
+	  return {
+	    salaryPeriod: payDayLastMonth.getMonth(),
+	    salaryDate: payDayLastMonth
+	  };
+	}
+  }
---
+++ public/scripts/scripts.js
@@ -0,0 +1,97 @@
+const socket = io();
+var _expenses;
+var _isLoaded = false;
+var _salaryPeriod;
+
+const $topCategoriesTable = document.getElementById("topCategoriesTable");
+const expenseTemplate = document.querySelector("#expenseTemplate").innerHTML;
+
+const $topExpensesTable = document.getElementById("topExpensesTable");
+
+const $nonFixedSumContainer = document.getElementById("nonFixedSumContainer");
+const nonFixedSumTemplate = document.querySelector("#nonFixedSumTemplate").innerHTML;
+
+socket.on("expenses", (expenses) => {
+
+    if (_isLoaded) return;
+    
+    _expenses = expenses;
+	let categorySummary = groupExpensesByCategory(expenses);
+	
+	for (var key of Object.keys(categorySummary)) {
+		var html = Mustache.render(expenseTemplate, {
+		  description: key,
+		  sum: Math.round(categorySummary[key],2) + " kr"
+		});
+        $topCategoriesTable.insertAdjacentHTML("beforeend", html);
+	  }
+
+    var html2 = Mustache.render(nonFixedSumTemplate, {
+        sum: Math.round(calculateNonFixedCosts(expenses)) + " kr"
+    });
+    
+    $nonFixedSumContainer.insertAdjacentHTML("beforeend", html2)
+
+    let topExpenses = getTopSingleExpenses(expenses);
+
+    for (var expense of topExpenses) {
+        var html = Mustache.render(expenseTemplate, {
+          description: expense.description,
+          sum: -1*Math.round(expense.sum,2) + " kr"
+        });
+        $topExpensesTable.insertAdjacentHTML("beforeend", html);
+    }
+
+    _isLoaded = true;
+    
+})
+
+socket.on("salaryPeriod",(salaryPeriod) => {
+    _salaryPeriod = salaryPeriod;
+    let DaysSinceSalary = Math.ceil((new Date() - new Date(salaryPeriod.salaryDate)) / (1000 * 60 * 60 * 24));
+    document.querySelector("#daysSinceSalary").innerText = DaysSinceSalary;
+})
+
+function calculateNonFixedCosts(expenses) {
+    return expenses.reduce((total, expense) => {
+        if (!expense.isFixedExpense && !expense.description.includes("Sparande") && expense.sum < 0) {
+            return total + expense.sum;
+        }
+        return Math.round(total,2);
+    }, 0);
+}
+
+function groupExpensesByCategory(expenses) {
+    const result = {};
+    
+    expenses.forEach(expense => {
+        if (!expense.isFixedExpense && !expense.category.includes("Sparande")) {
+            if (!result[expense.category]) {
+                result[expense.category] = 0;
+            }
+            result[expense.category] += expense.sum;
+        }
+    });
+    
+    // Convert object to array, sort, and take 5 biggest
+    const sortedCategories = Object.entries(result)
+        .sort(([,sumA], [,sumB]) => sumA - sumB)  // Sort in ascending order since numbers are negative
+        .slice(0, 5);
+
+    // Convert back to object
+    const topCategories = Object.fromEntries(sortedCategories);
+
+    return topCategories;
+}
+
+function getTopSingleExpenses(expenses) {
+    const filteredExpenses = expenses.filter(expense => 
+        !expense.isFixedExpense && !expense.category.includes("Sparande") && expense.sum < 0
+    );
+
+    // Sort expenses by sum in descending order and take 5 biggest
+    const topExpenses = filteredExpenses.sort((a, b) => a.sum - b.sum)
+        .slice(0, 5);
+
+    return topExpenses;
+}
